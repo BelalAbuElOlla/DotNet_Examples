1e14b5239265f26ac31b0e1015561853164efec5
Belal.Hassan <belal.m.abu.elolla.hassan@gmail.com>
2019-05-14 09:33:39 +0200

    GradeBook C# example

---
+++ GradeBook/src/GradeBook/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(String name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void showStatistics()
+        {
+            double minValue = double.MaxValue;
+            double maxValue = double.MinValue;
+            var result = 0.0;
+            foreach (var grade in grades)
+            {
+                if (grade < minValue) minValue = grade;
+                if (grade > maxValue) maxValue = grade;
+
+                result += grade;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"MaxValue is {maxValue}");
+            Console.WriteLine($"minValue is {minValue}");
+            Console.WriteLine($"Average is {result}");
+
+        }
+        public void addGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        String name;
+        List<double> grades;
+    }
+}
---
+++ GradeBook/src/GradeBook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("LiveBook");
+            book.addGrade(55.5);
+            book.addGrade(20.0);
+            book.addGrade(10.0);
+            book.showStatistics();
+        }
+    }
+}
